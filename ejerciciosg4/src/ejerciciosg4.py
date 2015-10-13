5be301fdcff27d9eeff035c49f9c68ce6d556a80
MAYKOL <MAYKOL@192.168.26.122>
2015-10-13 11:04:55 -0500

    identifica si es numero par o impar

---
+++ ejerciciosg4/src/ejerciciosg4.py
@@ -0,0 +1,14 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "MAYKOL"
+__date__ = "$13/10/2015 10:55:33 AM$"
+
+
+num=int(raw_input('introduce algun numero entero ')) 
+
+if num%2 == 0:
+ print 'el numero ',num , 'es par'
+if num%2 != 0:
+ print 'el numero ',num ,'es inpar' 
